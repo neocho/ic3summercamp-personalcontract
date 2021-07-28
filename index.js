970296c695b5169d6f48dcf8493b6ebeb5096fe4
Neo <choneo.ch@gmail.com>
2021-07-28 02:59:20 -0400

    test contract

--- index.js
+++ index.js
@@ -1,31 +1,69 @@
-function createContract() {
-  
-}
+let contractAddress = "0x74Cc97F1C0B94B2491B64033081358b106A79C66"; 
+let abi_json = {"result":"[{\"inputs\":[],\"name\":\"retrieve\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"num\",\"type\":\"uint256\"}],\"name\":\"store\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"};
+let user; 
+let personalContract; 
 
-function getBalance() {
-    let address = document.getElementById("address").value
+async function createContract() {
+    if(user){
+        let accounts = await web3.eth.getAccounts(); 
+        let usersAccount = accounts[0];
 
-    try {
-        web3.eth.getBalance(address, function (error, result) {
-            if (!error) {
-                document.getElementById("output").innerHTML = result + "wei";
-            }
+        let contract_abi = JSON.parse(abi_json.result);
+
+        personalContract = new web3.eth.Contract(contract_abi, contractAddress, {
+            from: usersAccount, 
         });
-    } catch (err) {
-        document.getElementById("output").innerHTML = err;
+
+    }else {
+        console.log("No Wallet Connected.");
+    }
+
+
+    document.getElementById("createContractButton").innerHTML = "Contract Created";
+
+    console.log("Contract Created");
+
+}
+
+async function setValue() {
+    const sentValue = await personalContract.methods.store("1").send();
+    document.getElementById("storeButton").innerHTML = sentValue;
+}
+
+async function getValue() {
+    const storedValue = await personalContract.methods.retrieve().call();
+    document.getElementById("outputValue").innerHTML = storedValue;
+}
+
+async function connectWallet() {
+    if(window.ethereum){
+        window.web3 = new Web3(ethereum);
+
+        try {
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts'});
+            user = accounts[0];
+            console.log(user);
+        }catch(error){
+            console.log("User Denied Access");
+        }
+
+        let firstHalf = user.substring(0, 5); 
+        let secondHalf = user.substring(user.length-5,user.length);
+
+        document.getElementById("connectWalletButton").innerHTML = firstHalf + "..." + secondHalf; 
+
+    }else{
+        console.log("Download Metamask or Ethereum compatible browser.");
     }
 }
 
-window.addEventListener("load", function() {
-    document.getElementById("createContractButton").onclick = createContract;
-    document.getElementById("getBalance").onclick = getBalance;
+window.onload = () => {
+    document.getElementById("connectWalletButton").addEventListener("click", connectWallet);
+    document.getElementById("createContractButton").addEventListener("click", createContract);
+    document.getElementById("storeButton").addEventListener('click', setValue);
+    document.getElementById("outputButton").addEventListener('click', getValue);
+};
 
-    if (typeof web3 !== 'undefined'){
-        console.log("Web3 Found: " + web3.currentProvider.constructor.name);
-        window.web3 = new Web3(web3.currentProvider);
-    }else{
-        console.log("No Web3 Found. Setting up a HTTP Provider"); 
-        window.web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/fd0f35f807c24ce1b0b5d34547fac7cc"));
-    }
 
-});
+
+
